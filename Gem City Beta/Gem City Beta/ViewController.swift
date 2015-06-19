15789aa7da7c69f2ccb0f98bfa5ad240bf2dc4c5
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2015-06-19 15:49:25 -0700

    Map Added
    First Commit

---
+++ Gem City Beta/Gem City Beta/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Gem City Beta
+//
+//  Created by Edrick Pascual on 6/19/15.
+//  Copyright (c) 2015 Edrick Pascual. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theMap: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
