143f265ceecd1739002945c07e32cca23ac39096
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2015-07-01 20:49:03 -0700

    Updated gem city
    Added sign up and login
    Updated annotations
    updated gem
    update startup page

--- Gem City Beta/Gem City Beta/VCMapView.swift
+++ Gem City Beta/Gem City Beta/VCMapView.swift
@@ -1,6 +1,6 @@
 //
 //  VCMapView.swift
-//  Gem City Beta
+//  Gem City
 //
 //  Created by Leslie Chicoine on 6/22/15.
 //  Copyright (c) 2015 Edrick Pascual. All rights reserved.
@@ -10,30 +10,27 @@ import MapKit
 
 extension ViewController: MKMapViewDelegate {
     
+    
     // 1
     func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
         if let annotation = annotation as? Trees {
             let identifier = "pin"
             var view: MKPinAnnotationView
-                        
+            
             if let dequeuedView = theMap.dequeueReusableAnnotationViewWithIdentifier(identifier)
                 as? MKPinAnnotationView {
-    // 2
+                    // 2
                     dequeuedView.annotation = annotation
                     view = dequeuedView
-                    
-                    
             } else {
-    // 3
+                // 3
                 view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
                 view.canShowCallout = false
                 view.calloutOffset = CGPoint(x: -5, y: 5)
                 view.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIView
                 view.image = UIImage(named: "gem.png")
-
             }
-                return view
-
+            return view
         }
         return nil
     }
